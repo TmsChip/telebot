f20399d0ade969384a3274f3c12d05aa42c4ce95
TmsChip <61943244+TmsChip@users.noreply.github.com>
2020-03-08 21:54:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const TelegramBot = require('node-telegram-bot-api')
-const TOKEN = '924855588:AAF6fdTMtqpdakBgggMn91ZD8zVIiVOUWf4'
-const bot = new TelegramBot(TOKEN, {
-    polling: true
-})
-bot.on('message', (msg) => {
-    console.log(msg)
-})
+// Подключаем библиотеку для работы с Telegram API в переменную
+var TelegramBot = require('node-telegram-bot-api');
+
+// Устанавливаем токен, который выдавал нам бот
+var token = '924855588:AAF6fdTMtqpdakBgggMn91ZD8zVIiVOUWf4';
+// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
+// Подробнее: https://core.telegram.org/bots/api#getupdates
+var bot = new TelegramBot(token, { polling: true });
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id; // Получаем ID отправителя
+    var resp = match[1]; // Получаем текст после /echo
+    bot.sendMessage(fromId, resp);
+});
+
+// Простая команда без параметров
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
+    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
+    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
+    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
+});
